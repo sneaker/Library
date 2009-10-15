74db294c1c36c04484be8d313c085ad97edef554
Thomas Kallenberg <thomas@no-more-secrets.ch>
2009-10-15 22:22:04 +0200

    Umlaut change

--- Library/src/test/CustomerTest.java
+++ Library/src/test/CustomerTest.java
@@ -7,14 +7,14 @@ public class CustomerTest extends TestCase {
 	
 	public void testCreateCustomer() {
 		Customer cu1 = new Customer("Keller", "Heinz");
-		cu1.setAdress("Zelgweg 12", 8000, "ZŸrich");
+		cu1.setAdress("Zelgweg 12", 8000, "ZÃ¼rich");
 		
 		assertEquals("Keller", cu1.getName());
 		assertEquals("Heinz", cu1.getSurname());
 		
 		assertEquals("Zelgweg 12",cu1.getStreet());
 		assertEquals(8000,cu1.getZip());
-		assertEquals("ZŸrich",cu1.getCity());
+		assertEquals("ZÃ¼rich",cu1.getCity());
 	}
 
 }
