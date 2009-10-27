f037947636713abf602845a1861c22acc666bb0c
Martin Schwab <mschwab@hsr.ch>
2009-10-27 01:27:26 +0100

    Refactored LookBackKeyBuffer.

--- Library/src/util/LookBackKeyBuffer.java
+++ Library/src/util/LookBackKeyBuffer.java
@@ -3,13 +3,28 @@ package util;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Knows about the last few characters the user entered on his keyboard and
+ * reports whether or not the typed text corresponds to an id of a book or user.
+ */
 public class LookBackKeyBuffer {
-	private static final int BUFFER_SIZE = 10;
+	/**
+	 * ID needs a fixed length because it would be difficult to make all book
+	 * id's prefix free to use them for the find-as-you-type feature. Change
+	 * constructor accordingly if value is set to more than 12 characters.
+	 */
+	private static final int ID_LENGTH = 3;
+	private static final String USER_ID_PREFIX = "us";
+	private static final String BOOK_ID_PREFIX = "bk";
+	private static final int BUFFER_SIZE = Math.max(USER_ID_PREFIX.length(),
+			BOOK_ID_PREFIX.length())
+			+ ID_LENGTH;
 	public StringBuffer buffer;
 	public int current;
 
 	public LookBackKeyBuffer() {
-		buffer = new StringBuffer("0000000000");
+		// Needed because int argument doesn't seem to work.
+		buffer = new StringBuffer("000000000000");
 		current = 0;
 	}
 
@@ -24,47 +39,38 @@ public class LookBackKeyBuffer {
 	}
 
 	public boolean isBookID() {
-		String bookIdRegex = "bk[0-9]{2}$";
-		Pattern pattern = Pattern.compile(bookIdRegex);
-		Matcher matcher = pattern.matcher(getLastString());
-		if (matcher.find())
-			return true;
-		return false;
+		return isMatching(BOOK_ID_PREFIX + "[0-9]{" + ID_LENGTH + "}$");
 	}
 
 	public boolean isUserID() {
-		String userIdRegex = "us[0-9]{2}$";
-		Pattern pattern = Pattern.compile(userIdRegex);
-		Matcher matcher = pattern.matcher(getLastString());
-		if (matcher.find())
-			return true;
-		return false;
+		return isMatching(USER_ID_PREFIX + "[0-9]{" + ID_LENGTH + "}$");
 	}
 
 	public boolean idTagStarted() {
-		String idRegex = "(bk|us)$";
-		Pattern pattern = Pattern.compile(idRegex);
-		Matcher matcher = pattern.matcher(getLastString());
-		if (matcher.find())
-			return true;
-		return false;
+		return isMatching(getBookOrUserRegex() + "$");
 	}
-	
+
 	public boolean idTagPending() {
-		String idRegex = "(bk|us)[0-9]{0,3}$";
-		Pattern pattern = Pattern.compile(idRegex);
-		Matcher matcher = pattern.matcher(getLastString());
-		if (matcher.find())
-			return true;
-		return false;
+		return isMatching(getBookOrUserRegex() + "[0-9]{0," + ID_LENGTH + "}$");
 	}
-	
+
 	public String getRecentId() {
-		String idRegex = "(bk|us)[0-9]{0,3}$";
-		Pattern pattern = Pattern.compile(idRegex);
+		Pattern pattern = Pattern.compile(getBookOrUserRegex() + "[0-9]{0,"
+				+ ID_LENGTH + "}$");
 		Matcher matcher = pattern.matcher(getLastString());
 		if (matcher.find())
 			return getLastString().substring(matcher.start());
 		return "";
+	}
+
+	private String getBookOrUserRegex() {
+		return "(" + BOOK_ID_PREFIX + "|" + USER_ID_PREFIX + ")";
+	}
+
+	private boolean isMatching(String regex) {
+		Matcher matcher = Pattern.compile(regex).matcher(getLastString());
+		if (matcher.find())
+			return true;
+		return false;
 	}
 }
