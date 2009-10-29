da4649d07dfb2d51730bc4541c7a28b461803d36
Martin Schwab <mschwab@hsr.ch>
2009-10-29 15:47:17 +0100

    Customers now displayed in result list.

--- Library/src/presentation/model/SearchResultListModel.java
+++ Library/src/presentation/model/SearchResultListModel.java
@@ -6,23 +6,34 @@ import javax.swing.ListModel;
 import javax.swing.event.ListDataListener;
 
 import domain.Book;
+import domain.Customer;
 import domain.Library;
+import domain.Loan;
+import domain.Searchable;
+import domain.Title;
 
 public class SearchResultListModel implements ListModel {
 
-	ArrayList<Book> list;
+	ArrayList<Searchable> list;
 	
 	public SearchResultListModel(Library library) {
-		list = new ArrayList<Book>();
+		list = new ArrayList<Searchable>();
 		// TODO: Display Results instead of fakes [Martin]
 		for (int i = 0; i < 10; i++)
 			list.add(library.getAvailableBooks().get(i));
+		Customer test = new Customer("Hans", "Tester");
+		test.setAdress("Testdrive 3", 6667, "Oklahoma");
+		library.getLoans().add(new Loan(test, new Book(new Title("jfkldsfls"))));
+		library.getLoans().add(new Loan(test, new Book(new Title("jfkldsfls1"))));
+		library.getLoans().add(new Loan(test, new Book(new Title("jfkldsfls2"))));
+		library.getLoans().add(new Loan(test, new Book(new Title("jfkldsfls3"))));
+		list.add(0, test);
 	}
 	
 	public void addListDataListener(ListDataListener l) {
 	}
 
-	public Book getElementAt(int index) {
+	public Searchable getElementAt(int index) {
 		return list.get(index);
 	}
 
--- Library/src/presentation/view/ResultCellRenderer.java
+++ Library/src/presentation/view/ResultCellRenderer.java
@@ -1,29 +1,21 @@
 package presentation.view;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Component;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.io.File;
 
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.ListCellRenderer;
 
-import util.TextUtils;
 import domain.Book;
+import domain.Customer;
 import domain.Library;
 
 public class ResultCellRenderer implements ListCellRenderer {
 
 	private static final int TEXT_WIDTH_DIFFERENCE = 100;
-	private static final Color SHINY_BLUE = new Color(0xADD8E6);
-	private static final String TITLE_FORMAT = "<html><p style='font-size:14pt; padding-left: 1.25cm; text-indent: -1cm;'>";
-	private int preferredWidth;
-	private final Library library;
+
+	private int preferredWidth = 250;
+	private Library library;
 
 	public ResultCellRenderer(Library library) {
 		this.library = library;
@@ -39,71 +31,22 @@ public class ResultCellRenderer implements ListCellRenderer {
 			index = list.getSelectedIndex();
 		}
 
-		// TODO: We need a different cell renderer for displaying customers [Martin]
-		Book selectedBook = (Book) list.getModel().getElementAt(index);
-
-		RenderPanel c = new RenderPanel(selectedBook, isSelected);
-		c.setLayout(new BorderLayout());
-		JLabel l = new JLabel(getFormattedTitle(selectedBook));
-		String bookImage = "img" + File.separatorChar + "book.png";
-		l.setIcon(new ImageIcon(bookImage));
-		l.setFont(new Font(null, Font.PLAIN, 14));
-		c.add(l, BorderLayout.WEST);
-
-		c.setBackground(Color.WHITE);
-		if (isSelected)
-			c.setBackground(SHINY_BLUE);
-
-		return c;
+		return getRenderedCell(value, isSelected);
 	}
 
-	private String getFormattedTitle(Book selectedBook) {
-		return TITLE_FORMAT
-				+ "<b>"
-				+ TextUtils.cutText(selectedBook.getTitle().getName(),
-						preferredWidth, TITLE_FORMAT + "<b>")
-				+ "</b><br />Autor: " + selectedBook.getTitle().getAuthor()
-				+ "<br />Verlag: " + selectedBook.getTitle().getPublisher();
+	private JPanel getRenderedCell(Object value, boolean isSelected) {
+		JPanel cell = new JPanel();
+		if (value instanceof Book)
+			cell = new ResultCellBookPanel((Book) value, isSelected,
+					preferredWidth, library);
+		else if (value instanceof Customer)
+			cell = new ResultCellUserPanel((Customer) value, isSelected,
+					preferredWidth, library);
+		return cell;
 	}
 
 	public void setPreferredWidth(int width) {
 		this.preferredWidth = Math.max(100, width - TEXT_WIDTH_DIFFERENCE);
 	}
 
-	private class RenderPanel extends JPanel {
-		private static final long serialVersionUID = -8035455214107649755L;
-		private final Book active;
-		private final boolean isSelected;
-
-		public RenderPanel(Book active, boolean isSelected) {
-			this.active = active;
-			this.isSelected = isSelected;
-		}
-
-		@Override
-		public void paint(Graphics g) {
-			super.paint(g);
-			String image = "";
-			if (!library.isBookLent(active))
-				image = "img/check16x16.png";
-			if (active.getCondition().equals(Book.Condition.DAMAGED))
-				image = "img/warning16x16.png";
-			if (active.getCondition().equals(Book.Condition.WASTE))
-				image = "img/exclamation16x16.png";
-			g.drawImage(new ImageIcon(image).getImage(), 43, 38, null);
-
-			if (!isSelected)
-				return;
-			if (library.isBookLent(active)) {
-				g.drawImage(new ImageIcon("img/agenda32x32.png").getImage(),
-						getWidth() - 60, getHeight() - 37, null);
-				g.drawImage(new ImageIcon("img/return32x32.png").getImage(),
-						getWidth() - 100, getHeight() - 37, null);
-				return;
-			}
-			g.drawImage(new ImageIcon("img/add32x32.png").getImage(),
-					getWidth() - 60, getHeight() - 37, null);
-		}
-
-	}
 }
