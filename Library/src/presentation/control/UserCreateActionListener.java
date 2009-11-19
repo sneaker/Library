0c9995d0e4de70c649dcf78060d7cb2c763fd77a
Martin Schwab <mschwab@hsr.ch>
2009-11-19 02:12:32 +0100

    added create user button on search action.

---
+++ Library/src/presentation/control/UserCreateActionListener.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package presentation.control;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import presentation.model.ModelController;
+import domain.Customer;
+
+public class UserCreateActionListener implements ActionListener {
+	private final ModelController controller;
+
+	public UserCreateActionListener(ModelController controller) {
+		this.controller = controller;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		Customer add = controller.library.createAndAddCustomer("Neuer", "Benutzer");;
+		add.setAdress("Strasse", 1234, "Ort");
+		controller.activeuser_model.setActiveUser(add);
+		controller.usertab_model.setEditing(true);
+		controller.status_model.setTempStatus("Neuer Benutzer erstellt");
+	}
+}
